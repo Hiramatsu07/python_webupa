f808cbb5773a44352709b35d01634871ca7ffd34
Hiram Verdefronte <hiram.verdefronte@upa.edu.py>
2022-09-15 09:26:52 -0400

    arreglao

--- app.py
+++ app.py
@@ -1,50 +1,61 @@
-from email.policy import default
 from flask import Flask, redirect
+
 from resources.task import Task
 from flask_restful import Api
 from flasgger import Swagger
+
 from db import db
+
 import os
-app = Flask(__name__)  
 
-api = Api(app,errors ={
-   ''   
+app = Flask(__name__)
+
+api = Api(app, errors={
+   ''
 })
-PREFIX = os.environ.get('PREFIX_PATH', '/api')
 
+PREFIX = os.environ.get('PREFIX_PATH', '/api')
+ 
 # Swagger config
 app.config['SWAGGER'] = {
-    'title': 'todo-backend',
-    'version': '1.0.0',
-    'description': 'API de servicios REST en Flask',
-    'uiversion': 2,
-    'tags': [{'name': 'jwt'}],
-    'specs': [{
-        'endpoint': 'apispec_1',
-        'route': f'{PREFIX}/apispec_1.json',
-        'rule_filter': lambda rule: True, # all in
-        'model_filter': lambda tag: True # all in
-    }],
-    'specs_route': f"{PREFIX}/apidocs/",
-    'static_url_path': f'{PREFIX}/static'
+   'title': 'todo-backend',
+   'version': '1.0.0',
+   'description': 'API de servicios REST en Flask',
+   'uiversion': 2,
+   'tags': [{'name': 'jwt'}],
+   'specs': [{
+       'endpoint': 'apispec_1',
+       'route': f'{PREFIX}/apispec_1.json',
+       'rule_filter': lambda rule: True,  # all in
+       'model_filter': lambda tag: True  # all in
+   }],
+   'specs_route': f"{PREFIX}/apidocs/",
+   'static_url_path': f'{PREFIX}/static'
 }
 swagger = Swagger(app)
-def env_config(name, defaul):
-    app.config[name] = os.environ.get(name, default = default)
-#database config 
-env_config('SQlALCHEMY_DATABASE_URI', 'postgresql://postgres@localhost:5432/todo')
-app.config['SQLLACHEMY_TRACK_MODIFICATIONS'] = False
+
+# Function to facilitate the app configuration from environment variables
+def env_config(name, default):
+   app.config[name] = os.environ.get(name, default=default)
+
+#Database config
+env_config('SQLALCHEMY_DATABASE_URI','postgresql://postgres:12345678@localhost:5432/todo')
+
+#SQLAlchemy config
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['PROPAGATE_EXCEPTIONS'] = True
 app.config['SQLALCHEMY_ECHO'] = False
+
 
 @app.route('/')
 @app.route(f'{PREFIX}')
 def welcome():
-    return redirect (f"{PREFIX}/apidocs", code=302)
+   return redirect(f"{PREFIX}/apidocs", code=302)
+ 
+api.add_resource(Task, f'{PREFIX}/task/<id>')
 
-api.add_resource(Task,f'{PREFIX}/task/<id>')
 
-#Bloque opcional para ejecutar con python app.py
+# Bloque opcional para ejecutar con python app.py
 if __name__ == '__main__':
     db.init_app(app)
     app.run()
--- models/task.py
+++ models/task.py
@@ -3,23 +3,24 @@ from db import db
 class TaskModel(db.Model):
     __tablename__ = 'task'
 
-    id = db.Column(db.Integer, primary_key = True)
-    description= db.Column(db.String)
+    id = db.Column(db.Integer, primary_key=True)
+    descrip = db.Column(db.String)
     status = db.Column(db.String)
-    def __init__(self, id, description, status):
-        self.id = id
-        self.description = description,
-        self.status = status
 
+    def __init__(self, id, descrip, status):
+        self.id = id
+        self.descrip = descrip
+        self.status = status
 
     def json(self, depth =0):
         json = {
-            'id': self.id, 
-            'description': self.description,
+            'id': self.id,
+            'descrip': self.descrip,
             'status': self.status
         }
-        return json
 
+        return json
+    
     @classmethod
     def find_by_id(cls, id):
-        return cls.query.filter_by(id = id).first()
+        return cls.query.filter_by(id=id).first()
